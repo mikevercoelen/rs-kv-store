9442ca8ef6bb4a6b461ad2bbe90c89217a02aa78
Mike Vercoelen <mike.vercoelen@gmail.com>
2022-01-18 19:36:41 +0100

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,9 @@ impl Database {
             let mut chunks = line.splitn(2, '\t');
             let key = chunks.next().expect("No key!");
             let value = chunks.next().expect("No value!");
-            map.insert(key, value);
+            map.insert(key.to_owned(), value.to_owned());
         }
 
-        Ok(Database {
-            map: HashMap::new()
-        })
+        Ok(Database { map: map })
     }
 }
