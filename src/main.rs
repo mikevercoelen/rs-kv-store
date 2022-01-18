a8481db5e072df214e2ca35c835d2c3da4d0d814
Mike Vercoelen <mike.vercoelen@gmail.com>
2022-01-18 17:13:50 +0100

    Babysteps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let mut arguments = std::env::args().skip(1);
+    let key = arguments.next().unwrap();
+    let value = arguments.next().unwrap();
+    let contents = format!("{}/t{}/n", key, value);
+
+    std::fs::write("kv.db", contents);
 }
