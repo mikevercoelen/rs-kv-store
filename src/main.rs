46719d6f465d83808498c68d65d7048f19b14e11
Mike Vercoelen <mike.vercoelen@gmail.com>
2022-01-18 17:15:20 +0100

    Fixed escapes

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let mut arguments = std::env::args().skip(1);
     let key = arguments.next().expect("Key was undefined");
     let value = arguments.next().expect("Value was undefined");
-    let contents = format!("{}/t{}/n", key, value);
+    let contents = format!("{}\t{}\n", key, value);
 
     std::fs::write("kv.db", contents);
 }
