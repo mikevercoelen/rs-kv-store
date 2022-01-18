9fb4720c1d6a6949af748dd31f89650e406a8fe8
Mike Vercoelen <mike.vercoelen@gmail.com>
2022-01-18 19:32:59 +0100

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
+use std::collections::HashMap;
+
 fn main() {
     let mut arguments = std::env::args().skip(1);
     let key = arguments.next().expect("Key was undefined");
     let value = arguments.next().expect("Value was undefined");
     let contents = format!("{}\t{}\n", key, value);
     std::fs::write("kv.db", contents).unwrap();
+
+    let database = Database::new();
+}
+
+struct Database {
+    map: HashMap<String, String>
+}
+
+impl Database {
+    fn new () -> Result<Database, std::io::Error> {
+        let mut map = HashMap::new();
+        let contents = std::fs::read_to_string("kv.db")?;
+
+        for line in contents.lines() {
+            let mut chunks = line.splitn(2, '\t');
+            let key = chunks.next().expect("No key!");
+            let value = chunks.next().expect("No value!");
+            map.insert(key, value);
+        }
+
+        Ok(Database {
+            map: HashMap::new()
+        })
+    }
 }
