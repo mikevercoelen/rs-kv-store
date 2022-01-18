fbac6ed0593195a83e7d1e66b7e7966c89d01a5d
Mike Vercoelen <mike.vercoelen@gmail.com>
2022-01-18 17:14:57 +0100

    Improved error messages

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut arguments = std::env::args().skip(1);
-    let key = arguments.next().unwrap();
-    let value = arguments.next().unwrap();
+    let key = arguments.next().expect("Key was undefined");
+    let value = arguments.next().expect("Value was undefined");
     let contents = format!("{}/t{}/n", key, value);
 
     std::fs::write("kv.db", contents);
